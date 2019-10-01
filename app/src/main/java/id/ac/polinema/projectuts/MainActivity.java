c5e0718f3a4f09eb2b4ef220601b0f117f4c22e2
jnparamitha <jnparamitha@gmail.com>
2019-10-01 21:07:58 +0700

    ProjectUTS1

---
+++ app/src/main/java/id/ac/polinema/projectuts/MainActivity.java
@@ -0,0 +1,22 @@
+package id.ac.polinema.projectuts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void handlerClickKatalogBaju(View view)
+    {
+        Intent intent = new Intent(this, KatalogBajuActivity.class);
+        startActivity(intent);
+    }
+}
