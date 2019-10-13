987fa3591860c9fe2eb34c86932fe265955fb490
jnparamitha <jnparamitha@gmail.com>
2019-10-13 21:44:08 +0700

    ProjectUTS4

--- app/src/main/java/id/ac/polinema/projectuts/KatalogTotebagActivity.java
+++ app/src/main/java/id/ac/polinema/projectuts/KatalogTotebagActivity.java
@@ -1,8 +1,12 @@
 package id.ac.polinema.projectuts;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+
+import id.ac.polinema.projectuts.fragments.KatalogWomenFragment;
 
 public class KatalogTotebagActivity extends AppCompatActivity {
 
@@ -11,4 +15,5 @@ public class KatalogTotebagActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_katalog_totebag);
     }
+
 }
